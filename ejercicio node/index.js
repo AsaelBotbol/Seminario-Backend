8c4c4147b8e5720cffcd1c42b9ace7d6ffbfa2c5
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-09-15 15:50:57 -0300

    dou

--- ejercicio node/index.js
+++ ejercicio node/index.js
@@ -1,1 +1,13 @@
-console.log("Messi");
+const express = require("express");
+const app = express()
+const port = 9000;
+
+
+
+app.get("/", (req, res) => {
+    res.send("API arrancó");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
+})
